8ed52f69d459d905a54094a179580950850134cc
Ben Dixon <benjdixon96@yahoo.co.uk>
2024-06-20 16:35:52 +0100

    Strings

--- Strings/src/main/java/com/sparta/bd/App.java
+++ Strings/src/main/java/com/sparta/bd/App.java
@@ -1,2 +1,27 @@
-package com.sparta.bd;public class App {
+package com.sparta.bd;
+
+
+
+public class App {
+    public static void main(String[] args) {
+        String string1 = "hello";
+        String string2 = "hello";
+        String string3 = new String("hello");
+        String string4 = new String("hello");
+        System.out.println(string1.hashCode());
+        System.out.println(string2.hashCode());
+        System.out.println(string3.hashCode());
+        System.out.println(string4.hashCode());
+        System.out.println(string1 == string2);
+        System.out.println(string3 == string4);
+        System.out.println(System.identityHashCode(string1));
+        System.out.println(System.identityHashCode(string2));
+        System.out.println(System.identityHashCode(string3));
+        System.out.println(System.identityHashCode(string4));
+
+
+
+
+
+    }
 }
--- Strings/src/main/java/com/sparta/bd/StringExample.java
+++ Strings/src/main/java/com/sparta/bd/StringExample.java
@@ -1,2 +1,14 @@
-package com.sparta.bd;public class StringExample {
+package com.sparta.bd;
+
+public class StringExample {
+    public static void main(String[] args) {
+        String string1 = "hello";
+        String upperCase = string1.toUpperCase();
+        System.out.println(upperCase);
+        String string = String.valueOf(100);
+        System.out.println(string1.toCharArray());
+
+
+
+    }
 }
