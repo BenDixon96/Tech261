7478ab73c75e7e47e2e879cdd688a7e0614ed870
Ben Dixon <benjdixon96@yahoo.co.uk>
2024-06-20 16:37:50 +0100

    Palindrome Kata

--- Strings/src/main/java/com/sparta/bd/StringKata.java
+++ Strings/src/main/java/com/sparta/bd/StringKata.java
@@ -1,2 +1,60 @@
-package com.sparta.bd;public class StringKata {
+package com.sparta.bd;
+
+import static java.lang.Character.isLetter;
+
+public class StringKata {
+
+    public static boolean isPalindrome(String word){
+        boolean result = false;
+        if(word.length() < 3){
+            return false;
+        }
+
+        StringBuilder reverse = new StringBuilder(word.toLowerCase()).reverse();
+        if(word.toLowerCase().contentEquals(reverse)){
+            result = true;
+        }
+
+        return result;
+
+    }
+
+    public static String removeSpecialChar(String sentence){
+        char[] sentenceArray = sentence.toCharArray();
+        StringBuilder result = new StringBuilder();
+
+        for(char letter: sentenceArray){
+            if(isLetter(letter) || String.valueOf(letter).contentEquals(" ")){
+                result.append(letter);
+            }
+        }
+        return String.valueOf(result);
+    }
+
+
+    public static String longestPalindromeInSentence(String sentence){
+        String[] sentenceArray = removeSpecialChar(sentence).split(" ");
+        StringBuilder result =  new StringBuilder("there are no palindromes");
+
+        int longestLength = 2;
+        for(String word: sentenceArray){
+
+            if(isPalindrome(word)){
+
+                if(word.length() > longestLength){
+                    result = new StringBuilder(word);
+                    longestLength = word.length();
+                } else if (word.length() == longestLength && !String.valueOf(result).contains(word)) {
+                        result.append(" ");
+                        result.append(word);
+
+                }
+
+            }
+        }
+        return String.valueOf(result);
+
+
+    }
+
 }
--- Strings/src/test/java/com/sparta/bd/StringKataTest.java
+++ Strings/src/test/java/com/sparta/bd/StringKataTest.java
@@ -1,2 +1,125 @@
-package com.sparta.bd;public class StringKataTest {
+package com.sparta.bd;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StringKataTest {
+    @Test
+    void givenStringReturnsFalse(){
+        String input = "";
+        Assertions.assertFalse(StringKata.isPalindrome(input));
+
+
+    }
+    @Test
+    void givenWordFewerThanThreeLettersReturnsFalse(){
+        String input = "aa";
+        Assertions.assertFalse(StringKata.isPalindrome(input));
+
+
+    }
+    @Test
+    void givenPalindromeReturnTrue(){
+        String input = "bob";
+        Assertions.assertTrue(StringKata.isPalindrome(input));
+
+
+    }
+    @Test
+    void givenLongWordNotPalindromeReturnsFalse(){
+        String input = "bobbby";
+        Assertions.assertFalse(StringKata.isPalindrome(input));
+
+
+    }
+    @Test
+    void givenLongWordPalindromeReturnsTrue(){
+        String input = "atoyotatoyota";
+        Assertions.assertTrue(StringKata.isPalindrome(input));
+
+
+    }
+    @Test
+    void givenLongerWordPalindromeReturnsTrue(){
+        String input = "gohangasalamiimalasagnahog";
+        Assertions.assertTrue(StringKata.isPalindrome(input));
+
+
+    }
+    @Test
+    void givenLongerWordPalindromeCheckCaseReturnsTrue(){
+        String input = "gOhangasalamiimalasagnahog";
+        Assertions.assertTrue(StringKata.isPalindrome(input));
+
+
+
+
+
+    }
+    @Test
+    void givenASentenceWithOnePalindromeReturnsPalindrome(){
+        String input = "hello bob";
+        Assertions.assertEquals("bob", StringKata.longestPalindromeInSentence(input));
+
+    }
+    @Test
+    void givenASentenceWithNoPalindromeReturnsNoPaindrome(){
+        String input = "hello billy";
+        Assertions.assertEquals("there are no palindromes", StringKata.longestPalindromeInSentence(input));
+
+    }
+    @Test
+    void givenASentenceWithMorePalindromesReturnsTheLongest(){
+        String input = "hello bob the if,i,had,a,hifi bob";
+        Assertions.assertEquals("ifihadahifi", StringKata.longestPalindromeInSentence(input));
+
+    }
+    @Test
+    void givenASentenceWithTwoOfTheSameLengthPalindromesReturnsBoth(){
+        String input = "hello bob the pop";
+        Assertions.assertEquals("bob pop", StringKata.longestPalindromeInSentence(input));
+
+    }
+    @Test
+    void givenASentenceWithTwoOfTheSamePalindromesReturnsOne(){
+        String input = "hello bob the bob";
+        Assertions.assertEquals("bob", StringKata.longestPalindromeInSentence(input));
+
+    }
+    @Test
+    void removeAllNonAphaCharFromAString(){
+        String input = "hello@@";
+        Assertions.assertEquals("hello", StringKata.removeSpecialChar(input));
+
+
+    }
+    @Test
+    void removeAllNonAphaCharFromAStringButNotSpaces(){
+        String input = "hello@@ hello";
+        Assertions.assertEquals("hello hello", StringKata.removeSpecialChar(input));
+
+
+    }
+    @Test
+    void givenAPlaindromeAndSpecialCharactersReturnPalindrome(){
+        String input = "123 @@ bo@b";
+        Assertions.assertEquals("bob", StringKata.longestPalindromeInSentence(input));
+
+
+    }
+    @Test
+    void givenALoadOfPalindromes() {
+        String input = "123 @@ never,odd,or,even madam,im,adam";
+        Assertions.assertEquals("neveroddoreven", StringKata.longestPalindromeInSentence(input));
+    }
+    @Test
+    void givenALongerPalindrome() {
+        String input = "123 @@ never,odd,or,even go,hang,a,salami,im,a,lasagna,hog";
+        Assertions.assertEquals("gohangasalamiimalasagnahog", StringKata.longestPalindromeInSentence(input));
+    }
+    @Test
+    void givenALoadMorePalindromesChecksIfCaseSensitve() {
+        String input = "so I told him did you know theres no,X,in,Nixon? and he said are you sure about that? ";
+        Assertions.assertEquals("noXinNixon", StringKata.longestPalindromeInSentence(input));
+    }
+
 }
